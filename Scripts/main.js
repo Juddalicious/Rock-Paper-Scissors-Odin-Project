2b47a0460dc45e68f815777f8a4c79e578972377
Juddalicious <3788mko0@gmail.com>
2021-08-25 20:43:23 +0900

    Added play round functuion and draw function, changed conditional to return message if no sting entered by user and to deal with draws

--- Scripts/main.js
+++ Scripts/main.js
@@ -7,32 +7,62 @@ randomNumber = Math.floor(Math.random()*3) + 1;
 //-- in the same function if one then return string 'rock', if two then 'paper', if three then 'scissor'
 
 if (randomNumber === 1) {
-    computerSelection = 'Rock';
+    computerSelection = 'rock';
 } else if (randomNumber === 2) {
-    computerSelection = 'Paper';
+    computerSelection = 'paper';
 } else if (randomNumber === 3) {
-        computerSelection = 'Scissors';
+        computerSelection = 'scissors';
 }
 
 console.log(computerSelection);
 
-//-- call function and asign to variable
 
-
+//-- create funtion to play one round
+function playRound() {
 
 //user makes choice using prompt and typing rock, paper, or scissor and saves to variable
-
 let userSelection = prompt('じゃんけんぽん!!', '');
 
 //change userSelection to lowercase
-
+if (userSelection != '') {
 userSelection = userSelection.toLowerCase();
 console.log(userSelection);
+}
 
 //conditinal determines who won, computer or user then console logs string congratulating or commiserating
-
 if ((userSelection === 'rock' && computerSelection === 'Scissors') || (userSelection === 'scissors' && computerSelection === 'Paper') || (userSelection === 'paper' && computerSelection === 'Rock')) {
     console.log('The battle was long but your brains and might prevailed. You won!');
+} else if (userSelection === computerSelection) {
+playDraw();
+} else if (userSelection === '') {
+    console.log('Scared bro? Reload and let\'s do this');
 } else {
     console.log('ROFL, Loser!');
+}
+}
+
+playRound();
+playRound();
+
+
+//function to run if there's a draw
+function playDraw() {
+
+    //user makes choice using prompt and typing rock, paper, or scissor and saves to variable
+let userSelection = prompt('It was a draw let\s go again big shlong! I got this!', '');
+
+//change userSelection to lowercase
+if (userSelection != '') {
+userSelection = userSelection.toLowerCase();
+console.log(userSelection);
+}
+
+//conditinal determines who won, computer or user then console logs string congratulating or commiserating
+if ((userSelection === 'rock' && computerSelection === 'Scissors') || (userSelection === 'scissors' && computerSelection === 'Paper') || (userSelection === 'paper' && computerSelection === 'Rock')) {
+    console.log('The battle was long but your brains and might prevailed. You won!');
+} else if (userSelection === '') {
+    console.log('Scared bro? Reload and let\'s do this');
+} else {
+    console.log('ROFL, Loser!');
+}
 }
