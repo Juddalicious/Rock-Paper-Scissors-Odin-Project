29e883e921f814cf4b171f17d5102495f782dcfe
Juddalicious <3788mko0@gmail.com>
2021-08-25 20:59:38 +0900

    Messed with loads of things. Put everything in a function that calls itself, dealt with input thats not rock paper or scissors

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,24 +1,21 @@
 
-//computer makes a random choice from rock, paper, or scissor and saves to variable
-//-- in a function create random number from one to three
+//create funtion to play one round
+function playRound() {
 
+//computer makes a random choice from rock, paper, or scissor and saves to variable
+//in a function create random number from one to three
 randomNumber = Math.floor(Math.random()*3) + 1;
 
-//-- in the same function if one then return string 'rock', if two then 'paper', if three then 'scissor'
-
+//in the same function if one then return string 'rock', if two then 'paper', if three then 'scissor'
 if (randomNumber === 1) {
-    computerSelection = 'rock';
+computerSelection = 'rock';
 } else if (randomNumber === 2) {
-    computerSelection = 'paper';
+computerSelection = 'paper';
 } else if (randomNumber === 3) {
-        computerSelection = 'scissors';
+computerSelection = 'scissors';
 }
 
 console.log(computerSelection);
-
-
-//-- create funtion to play one round
-function playRound() {
 
 //user makes choice using prompt and typing rock, paper, or scissor and saves to variable
 let userSelection = prompt('じゃんけんぽん!!', '');
@@ -30,25 +27,29 @@ console.log(userSelection);
 }
 
 //conditinal determines who won, computer or user then console logs string congratulating or commiserating
-if ((userSelection === 'rock' && computerSelection === 'Scissors') || (userSelection === 'scissors' && computerSelection === 'Paper') || (userSelection === 'paper' && computerSelection === 'Rock')) {
-    console.log('The battle was long but your brains and might prevailed. You won!');
+if ((userSelection === 'rock' && computerSelection === 'scissors') || (userSelection === 'scissors' && computerSelection === 'paper') || (userSelection === 'paper' && computerSelection === 'rock')) {
+alert('The battle was long but your brains and might prevailed. You won!');
 } else if (userSelection === computerSelection) {
 playDraw();
 } else if (userSelection === '') {
-    console.log('Scared bro? Reload and let\'s do this');
+alert('Scared bro? Reload and let\'s do this');
+} else if (userSelection != 'rock' && userSelection != 'scissors' && userSelection != 'paper') {
+alert("The options are 'rock' 'paper' or 'scissors you pillock. Again.");
+playRound();
 } else {
-    console.log('ROFL, Loser!');
+alert('ROFL, Loser!');
 }
+playRound();
 }
 
 playRound();
-playRound();
+
 
 
 //function to run if there's a draw
 function playDraw() {
 
-    //user makes choice using prompt and typing rock, paper, or scissor and saves to variable
+//user makes choice using prompt and typing rock, paper, or scissor and saves to variable
 let userSelection = prompt('It was a draw let\s go again big shlong! I got this!', '');
 
 //change userSelection to lowercase
@@ -59,10 +60,10 @@ console.log(userSelection);
 
 //conditinal determines who won, computer or user then console logs string congratulating or commiserating
 if ((userSelection === 'rock' && computerSelection === 'Scissors') || (userSelection === 'scissors' && computerSelection === 'Paper') || (userSelection === 'paper' && computerSelection === 'Rock')) {
-    console.log('The battle was long but your brains and might prevailed. You won!');
+alert('The battle was long but your brains and might prevailed. You won!');
 } else if (userSelection === '') {
-    console.log('Scared bro? Reload and let\'s do this');
+alert('Scared bro? Reload and let\'s do this');
 } else {
-    console.log('ROFL, Loser!');
+alert('ROFL, Loser!');
 }
 }
