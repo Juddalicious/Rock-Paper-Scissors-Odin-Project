f4b5c91f2757d128467072d8126ce94ce8e5c5b4
Juddalicious <3788mko0@gmail.com>
2021-11-07 04:04:39 +0900

    Got rid of unnecessary if statement for lowercase method and fixed draw function to run the random number thing

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,3 @@
-
 //create funtion to play one round
 function playRound() {
 
@@ -21,10 +20,7 @@ console.log(computerSelection);
 let userSelection = prompt('じゃんけんぽん!!', '');
 
 //change userSelection to lowercase
-if (userSelection != '') {
 userSelection = userSelection.toLowerCase();
-console.log(userSelection);
-}
 
 //conditinal determines who won, computer or user then console logs string congratulating or commiserating
 if ((userSelection === 'rock' && computerSelection === 'scissors') || (userSelection === 'scissors' && computerSelection === 'paper') || (userSelection === 'paper' && computerSelection === 'rock')) {
@@ -42,28 +38,46 @@ alert('ROFL, Loser!');
 playRound();
 }
 
-playRound();
-
-
 
 //function to run if there's a draw
 function playDraw() {
 
-//user makes choice using prompt and typing rock, paper, or scissor and saves to variable
-let userSelection = prompt('It was a draw let\s go again big shlong! I got this!', '');
+//computer chooses again
+randomNumber = Math.floor(Math.random()*3) + 1;
+
+if (randomNumber === 1) {
+computerSelection = 'rock';
+} else if (randomNumber === 2) {
+computerSelection = 'paper';
+} else if (randomNumber === 3) {
+computerSelection = 'scissors';
+}
+
+console.log(computerSelection);
+
+//user makes choice using new draw prompt
+let userSelection = prompt("It was a draw let\'s go again big shlong! I got this!", '');
 
 //change userSelection to lowercase
-if (userSelection != '') {
 userSelection = userSelection.toLowerCase();
-console.log(userSelection);
-}
+
 
 //conditinal determines who won, computer or user then console logs string congratulating or commiserating
 if ((userSelection === 'rock' && computerSelection === 'Scissors') || (userSelection === 'scissors' && computerSelection === 'Paper') || (userSelection === 'paper' && computerSelection === 'Rock')) {
 alert('The battle was long but your brains and might prevailed. You won!');
+} else if (userSelection === computerSelection) {
+    playDraw(); 
 } else if (userSelection === '') {
-alert('Scared bro? Reload and let\'s do this');
+    alert('Scared bro? Reload and let\'s do this');
+} else if (userSelection != 'rock' && userSelection != 'scissors' && userSelection != 'paper') {
+   alert("The options are 'rock' 'paper' or 'scissors you pillock. Again.");
+   playRound();
+} else if (userSelection === computerSelection) {
+    playDraw(); 
 } else {
 alert('ROFL, Loser!');
 }
+playRound();
 }
+
+playRound();
